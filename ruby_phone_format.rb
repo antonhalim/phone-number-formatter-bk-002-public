200580f5f16953c5eaa2a506d95b549aee2ea67d
axton21 <axton_n@yahoo.com>
2015-02-11 18:58:10 -0500

    get it done

--- ruby_phone_format.rb
+++ ruby_phone_format.rb
@@ -1,1 +1,15 @@
-# Write your code here
+require 'pry'
+def normalize_phone_number(number)
+    formatted = number.gsub(/\W/,"")
+
+    if formatted.length < 10
+      return number
+    else
+     "(#{formatted[0..2]}) #{formatted[3..5]}-#{formatted[6..9]}"
+    end
+
+# binding.pry
+
+end
+
+# /^\(?\d{3}[\)?\s?\-?]?\d{3}[\s?\-?]?\d{4}$/
